42bb50d4a01982b2ea814dd03020eeed0d51932d
jensineramirez <jensineramirez@users.noreply.github.com>
2019-04-26 21:46:33 -0700

    Completed some styling on contact form

--- AwesomeAirline/AwesomeAirline/JS/ContactScript.js
+++ AwesomeAirline/AwesomeAirline/JS/ContactScript.js
@@ -1,1 +1,7 @@
-﻿
+﻿/*
+ Resize based on content div -- if content div is above 80vh, give foot-container bottom 0
+ */
+$(document).ready(function () {
+    var contentSize = $(".content").outerHeight(true);
+    alert(contentSize);
+});
